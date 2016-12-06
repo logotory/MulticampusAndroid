e02f5fd8c84a83ccc0d08adc718a17b5e421d5f3
david.sun <david99.sun@gmail.com>
2016-12-06 17:27:12 +0900

    adding files

--- ch2-meterial/src/main/java/com/example/ch2_meterial/MainActivity.java
+++ ch2-meterial/src/main/java/com/example/ch2_meterial/MainActivity.java
@@ -1,13 +1,204 @@
 package com.example.ch2_meterial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+
+// tablayout 하위에 viewpager..
+// viewpager 유저 조정시 tab button 조정은 tablayout이 자동처리
+// tab button 유저 조정시 viewpager 조정은 개발자가
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener, View.OnClickListener{
+
+    ViewPager viewPager;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    boolean isDrawerOpend;
+
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    FloatingActionButton fab;
+    CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //actionbar 에 들어갈 내용을 위리의 View인 toolbar에 적용
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        //toggle에 들어가는 문자열은 화면 출력과 상관없다,
+        //accessibility 때문에..
+        //서브 클래스 만드는것은 이벤트 처리 하겠다는 의미..
+        //필수는 아니다.
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                isDrawerOpend = true;
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                isDrawerOpend = false;
+            }
+        };
+
+
+        drawerLayout.addDrawerListener(toggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //왼쪽에 아이콘 이미지 나오게 하는데 필요
+
+        //NavigationView의 메뉴 클릭 이벤트
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_drawer_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d("tag", "navigation view clicked");
+                return false;
+            }
+        });
+
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        //tablayout과 viewpager연결
+        //tab button을 직업 문자열, icon등으로 구성해도 되는데..
+        //viewpager와 연결되어 있다면.. viewpager화면 갯수만큼..viewpager
+        //화면 titile을 그대로 얻어 button으로 구성해 준다.
+        tabLayout.setupWithViewPager(viewPager);
+        //tabLayout.addOnTabSelectedListener(this);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coodinator);
+
+        collapsingToolbarLayout.setTitle("AppBar Title");
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Snackbar.make(coordinatorLayout, "I am SnackBar...", Snackbar.LENGTH_SHORT)
+                .setAction("MyAction", new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        Log.d("kkang", "Snackbar click...");
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        toggle.syncState();
+    }
+
+    //menu event 함수 .. toggle의 이벤트가 menu이벤트를 타서
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //drawer가 open된 상태이다.. 유저가 back button 누르면?
+    @Override
+    public void onBackPressed() {
+        if (isDrawerOpend)
+            drawerLayout.closeDrawers();
+        else
+            super.onBackPressed();
+    }
+
+    //menu 구현을 위해 자동으로 호출되는 함수
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //menu를 xml로 작성.. 초기화 시켜야 한다..
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //ViewPager를 위한 Adapter
+    //PagerAdapter, FragmentStatePagerAdapter 상속으로
+    class MyPagerAdapter extends FragmentStatePagerAdapter {
+        //항목 집합.. ViewPager의 항목은 화면한장.. Fragment로 표현
+        List<Fragment> fragments = new ArrayList<>();
+
+        //ViewPager title 문자열.. PagerTitleStrip, PagerTabStrip등에서
+        //이 문자열로 title구성
+        //우리의 경우는 tablayout에서..
+        String titles[] = new String[]{"TAB1", "TAB2", "TAB3"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm); //상위 클래스에 FramentManter 전달.
+            //우리는 각 항목을 위한 Fragmentn만 결정해 주면 상위클래스에서
+            //FragmentTransaction를 이용해 Fragment 제어
+
+            fragments.add(new OneFragment());
+            fragments.add(new TwoFragment());
+            fragments.add(new ThreeFragment());
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        //page의 title을 획득할 목적으로 자동 호출
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+
+        //유저에 의해 tab button 눌린순간 viewpager 화면 조정
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+
     }
 }
--- ch2-meterial/src/main/java/com/example/ch2_meterial/OneFragment.java
+++ ch2-meterial/src/main/java/com/example/ch2_meterial/OneFragment.java
@@ -1,12 +1,22 @@
 package com.example.ch2_meterial;
 
 
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,20 +24,124 @@ import android.widget.LinearLayout;
  */
 public class OneFragment extends Fragment {
 
+    RecyclerView recyclerView;
+
 
     public OneFragment() {
         // Required empty public constructor
     }
 
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        if (container == null) {
 
-            return null;
+        //Fragmemt 의 layout 초기화
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_one, container, false);
+
+        //항목 구성 가상 데이터 준비..
+        List<String> list = new ArrayList<>();
+        for(int i=0; i < 20; i++){
+            list.add("item=" +i);
         }
-        return (LinearLayout)inflater.inflate(R.layout.fragment_one, container, false);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(new MyAdapter(list));
+        recyclerView.addItemDecoration(new MyDecorator());
+
+        return recyclerView;
     }
 
+    //findbViewById의 성능이슈 .. find한 Viw를 메모리에 누적시키는 역할
+    //adapter에서 이 Holder객체를 메모리에 누적만 시킨다면 한번만 find
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        //한 항목에 find 대상이 되는 view를 나열..
+        public TextView titleView;
 
+        //holder를 사용하는 곳은 ..adapter이다. adapter에서
+        //항목 layout 계층 root를 매게변수로 전달..
+        public MyViewHolder(View root) {
+            super(root);
+
+            titleView = (TextView) root.findViewById(android.R.id.text1); //android에 있는 텍스트 넣었다.
+        }
+    }
+
+    //항목을 구성하기 위한 adapter..
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        List<String> list; //항목 데이터 집합객체 .. activity 가 전달할 거다
+
+        public MyAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        //각 항목을 위한 layoutchrl초기화..view 홀더 준비
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false); //문자열 하나 나오는 안드로이드 라이브러리 쓴거다.
+
+            return new MyViewHolder(view);
+        }
+
+        //항목 하나를 구성하기 위한 함수..
+        //여기에 개발자 로직이 들어간다.
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            String txt = list.get(position);
+            holder.titleView.setText(txt);
+        }
+    }
+
+    class MyDecorator extends RecyclerView.ItemDecoration {
+        //각 항목이 화면에 모두 찍힌후 그 위에 추가 꾸미기 작업
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            super.onDrawOver(c, parent, state);
+
+            //view의 사이즈 획득
+            int width = parent.getWidth();
+            int height = parent.getHeight();
+
+            //이미지 사이즈 획득
+            //가운데 위치시키려고
+            Drawable dr = getActivity().getResources().getDrawable(R.drawable.kbo);
+            int drWidth = dr.getIntrinsicWidth();
+            int drHeight = dr.getIntrinsicHeight();
+
+            //이미지가 그려질 left, top 좌표값 획득..
+            int left = width / 2  - drWidth/2;
+            int top = height/2 - drHeight/2;
+
+            //이미지를 화면에 그리고..
+            c.drawBitmap(BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.kbo),
+                    left, top, null);
+        }
+
+
+        //항목 하나하나를 꾸미기 위해서 호출
+        //항목 하나를 구성하기 위한 사각형 정보가 매게 변수로 전달..
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+
+            //항목의 index 값을 획득해서..
+            int index = parent.getChildAdapterPosition(view)+1;
+
+            if(index % 3 == 0){
+                outRect.set(20, 20, 20, 20);
+            }else {
+                outRect.set(20, 20, 20, 0);
+            }
+
+            view.setBackgroundColor(0xFFECE9E9); //배경색 회색 철
+            ViewCompat.setElevation(view, 5.0f); //음영처리. 하위 호환성 때문에 ViewCompat 사용
+        }
+    }
 }
